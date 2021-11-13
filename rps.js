0db03d1be76fcf99f05bf2deba17640f074d7502
Isaac Kabucho <kabszac@gmail.com>
2021-11-13 17:13:30 +0300

    added eventlistners to the code and more html

--- rps.js
+++ rps.js
@@ -1,6 +1,9 @@
 
 let compScore = 0
 let playerScore = 0
+const buttons = document.querySelectorAll("button")
+
+
 
 function computerPlay(){
     let items = ["rock", "paper", "scissors"]
@@ -8,39 +11,80 @@ function computerPlay(){
     return item
 }
 
-function playRound(playerSelection, computerSelection){
-  if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
-  (playerSelection == 'paper' && computerSelection == 'rock') || 
-  (playerSelection == 'scissors' && computerSelection == 'paper')){
-      playerScore += 1
-      return `You win ${playerSelection} beats ${computerSelection}`
-  }
-  else if(playerSelection == computerSelection){
-      return "You tie"
-  }
-  else {
-      compScore += 1
-      return `You lose ${computerSelection} beats ${playerSelection}`
-  }
-}
 
-function game(){
-    for (let i =0; i < 5; i++ ){
-        let ui = prompt("Enter your choice")
-        console.log(playRound(ui, computerPlay()))
-    }
-    if (playerScore > compScore){
-        console.log("You win!")
-    }
-    else if ( playerScore == compScore){
-        console.log("You Tie")
-    }
-    else{
-        console.log(" Oops You lose!")
-    }
-}
+// function playRound(playerSelection, computerSelection){
+//     if ((playerSelection == "rock" && computerSelection == 'scissors') ||
+//         (playerSelection == "paper" && computerSelection == "rock")  ||
+//         (playerSelection == "scissors" && computerSelection == "paper")){
+//             playerScore += 1
+//             return 'Y'
+//         }
+//   }
+//   else if(playerSelection == computerSelection){
+//       return "You tie"
+//   }
+//   else {
+//       compScore += 1
+//       //return `You lose ${computerSelection} beats ${playerSelection}`
+      
+      
+//   }
 
-game()
+// }
+
+function playRound(playerSelection){
+    let computerSelection = computerPlay()
+    output = ""
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
+        (playerSelection == 'paper' && computerSelection == 'rock') || 
+        (playerSelection == 'scissors' && computerSelection == 'paper')){
+            playerScore += 1
+            output = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + compScore)
+
+            if (playerScore == 5){
+                output += "<br> You Win the game"
+                setTimeout(()=>{location.reload(true)}, 3000)
+            }
+    }else if(playerSelection == computerSelection){
+        output = `you tie ${playerSelection} is same as ${computerSelection}`
+    }else {
+        compScore += 1
+        output = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+        + "<br><br>Player score: " + playerScore + "<br>Computer score: " + compScore)
+
+        if (compScore == 5){
+            output += ' <br> You lost the game'
+            setTimeout(()=>{location.reload(true)}, 3000)
+        }
+    }
+    document.querySelector('.result').innerHTML = output
+    return output
+  }
+
+// function game(){
+//     for (let i =0; i < 5; i++ ){
+//         let ui = prompt("Enter your choice")
+//         console.log(playRound(ui, computerPlay()))
+//     }
+//     if (playerScore > compScore){
+//         console.log("You win!")
+//     }
+//     else if ( playerScore == compScore){
+//         console.log("You Tie")
+//     }
+//     else{
+//         console.log(" Oops You lose!")
+//     }
+// }
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        playRound(btn.id)
+    })
+});
+
+// game()
 
 
 
